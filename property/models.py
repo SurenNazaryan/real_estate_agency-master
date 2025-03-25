53d717615c08e7b73de4009f9ca307b8e21c59b8
SurenNazaryan <127138768+SurenNazaryan@users.noreply.github.com>
2025-03-25 17:50:21 +0300

    correct fields params

--- property/models.py
+++ property/models.py
@@ -5,7 +5,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    new_building = models.BooleanField('Новостройка', null=True, blank=True)
+    new_building = models.BooleanField('Новостройка', null=True, blank=True, db_index=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -73,7 +73,7 @@ class Owner(models.Model):
     full_name = models.CharField('ФИО владельца:', max_length=200, db_index=True)
     phone_number = models.CharField('Номер владельца:', max_length=20, db_index=True)
     normalized_phone_number = PhoneNumberField('Нормализированный номер владельца:', blank=True, null=True, db_index=True)
-    flats = models.ManyToManyField(Flat, related_name='owners', verbose_name='Квартиры в собственности:')
+    flats = models.ManyToManyField(Flat, related_name='owners', verbose_name='Квартиры в собственности:', blank=True)
 
     def __str__(self):
         return self.full_name
