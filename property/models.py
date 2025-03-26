3ae236dca409789c0e9c10eb548dc0990edf9fcf
SurenNazaryan <127138768+SurenNazaryan@users.noreply.github.com>
2025-03-26 17:05:52 +0300

    Remove field phone_number from owner

--- property/models.py
+++ property/models.py
@@ -71,7 +71,6 @@ class Complaint(models.Model):
 
 class Owner(models.Model):
     full_name = models.CharField('ФИО владельца:', max_length=200, db_index=True)
-    phone_number = models.CharField('Номер владельца:', max_length=20, db_index=True)
     normalized_phone_number = PhoneNumberField('Нормализированный номер владельца:', blank=True, null=True, db_index=True)
     flats = models.ManyToManyField(Flat, related_name='owners', verbose_name='Квартиры в собственности:', blank=True)
 
